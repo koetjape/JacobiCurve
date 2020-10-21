ce6ab5b5865242a5d0f3b1b40116a31324af8307
HSEc0der <favarit99>
2020-10-21 13:31:59 +0300

    Initial Commit

---
+++ elliptic_curve/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  elliptic_curve
+//
+//  Created by Андрей Фомин on 21.10.2020.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
